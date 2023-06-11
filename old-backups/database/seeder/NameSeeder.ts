c32ebde26972862f41991eeca31afd21d0ec5f48
AnuzPandey <anuzbvbmaniac123@gmail.com>
2023-06-11 23:49:34 +0545

    :zap: modified: scripts to run with pnpm | schema structure addition.

---
+++ old-backups/database/seeder/NameSeeder.ts
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose'
+import Name from "../model/Name";
+
+// mongoose.connect('mongodb://localhost:27017/names')
+
+const namesData = [
+    {
+        name: ''
+    }
+]
