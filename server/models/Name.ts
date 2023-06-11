532a997464c1857ec0c60bf04399dc90e0a9a086
Hari Acharya <harryacharya2003@gmail.com>
2023-06-12 00:07:51 +0545

    bend db stucture

--- server/models/Name.ts
+++ server/models/Name.ts
@@ -1,16 +1,35 @@
-import mongoose from "mongoose";
+import { Schema, Document, model, SchemaTypes } from "mongoose";
 import { Gender } from "../types/Gender";
 
-const { Schema, SchemaTypes, model } = mongoose;
+interface NameTypes extends Document {
+  name: string;
+  name_np?: string;
+  meaning: string;
+  meaning_np?: string | null;
+  gender: String;
+  verifiedBy?: Schema.Types.ObjectId;
+  verifiedAt?: Date;
+  createdAt: Date;
+  createdBy?: Schema.Types.ObjectId;
+  updatedAt: Date;
+  deletedAt?: Date;
+}
 
-const nameSchema = new Schema({
+const nameSchema = new Schema<NameTypes>({
   name: String,
   name_np: {
     type: String,
     required: false,
   },
+  meaning: String,
+  meaning_np: {
+    type: String,
+    default: null,
+    required: false,
+  },
   gender: {
-    type: Gender,
+    type: String,
+    enum: [Gender.Male, Gender.Female, Gender.Others],
     default: Gender.Male,
   },
   verifiedBy: {
@@ -29,6 +48,6 @@ const nameSchema = new Schema({
   deletedAt: { type: Date, required: false },
 });
 
-const Name = model("Name", nameSchema);
+const Name = model<NameTypes>("Name", nameSchema);
 
 export default Name;
