146876331931a5f2af0b219b40059a1274a28996
AnuzPandey <anuzbvbmaniac123@gmail.com>
2023-06-15 00:40:34 +0545

    :sparkles: feature: adds dark and light mode switcher.

--- client/src/layouts/MainLayout.vue
+++ client/src/layouts/MainLayout.vue
@@ -1,10 +1,35 @@
 <script lang="ts" setup>
-import TopBgElement from "../components/Abstract/TopBgElement.vue";
+import TopBgElement from "../components/Abstract/TopBgElement.vue"
 import Logo from "../components/Logos/Logo.vue"
+import {onMounted, ref} from "vue"
+
+const darkMode = ref(false)
+
+onMounted(() => {
+  let themeStatus = localStorage.getItem('theme')
+
+  if (themeStatus === 'dark' || (themeStatus == null && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    darkMode.value = true
+    document.documentElement.classList.add('dark')
+  } else {
+    darkMode.value = false
+    document.documentElement.classList.remove('dark')
+  }
+})
+
+const toggleTheme = (event) => {
+  if (event.target.checked) {
+    document.documentElement.classList.add('dark')
+    localStorage.setItem('theme', 'dark')
+  } else {
+    document.documentElement.classList.remove('dark')
+    localStorage.setItem('theme', 'light')
+  }
+}
 </script>
 
 <template>
-  <div class="isolate bg-white">
+  <div class="isolate bg-white dark:bg-gray-950">
 
     <TopBgElement/>
 
@@ -17,9 +42,31 @@ import Logo from "../components/Logos/Logo.vue"
           </a>
         </div>
 
-        <div class="flex flex-1 justify-end">
-          <a class="flex gap-2" href="https://github.com/rbcrossley/randomNepaliNameGenerator" target="_blank">
-            <svg aria-hidden="true" class="h-6 w-6 fill-slate-900" viewBox="0 0 24 24">
+        <div class="flex flex-1 gap-x-4 justify-end">
+          <label class="swap swap-rotate">
+            <input
+              class="hidden"
+              type="checkbox"
+              name="theme"
+              id="theme"
+              @change="toggleTheme"
+              :checked="darkMode"
+            />
+
+            <!-- sun icon -->
+            <svg class="swap-on fill-current w-5 h-5 dark:fill-white" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z"/>
+            </svg>
+
+            <!-- moon icon -->
+            <svg class="swap-off fill-current w-5 h-5 dark:fill-white" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z"/>
+            </svg>
+
+          </label>
+
+          <a class="flex gap-2 dark:text-white" href="https://github.com/rbcrossley/randomNepaliNameGenerator" target="_blank">
+            <svg aria-hidden="true" class="h-6 w-6 fill-slate-900 dark:fill-white" viewBox="0 0 24 24">
               <path clip-rule="evenodd" d="M12 2C6.477 2 2 6.463 2 11.97c0 4.404 2.865 8.14 6.839 9.458.5.092.682-.216.682-.48 0-.236-.008-.864-.013-1.695-2.782.602-3.369-1.337-3.369-1.337-.454-1.151-1.11-1.458-1.11-1.458-.908-.618.069-.606.069-.606 1.003.07 1.531 1.027 1.531 1.027.892 1.524 2.341 1.084 2.91.828.092-.643.35-1.083.636-1.332-2.22-.251-4.555-1.107-4.555-4.927 0-1.088.39-1.979 1.029-2.675-.103-.252-.446-1.266.098-2.638 0 0 .84-.268 2.75 1.022A9.607 9.607 0 0 1 12 6.82c.85.004 1.705.114 2.504.336 1.909-1.29 2.747-1.022 2.747-1.022.546 1.372.202 2.386.1 2.638.64.696 1.028 1.587 1.028 2.675 0 3.83-2.339 4.673-4.566 4.92.359.307.678.915.678 1.846 0 1.332-.012 2.407-.012 2.734 0 .267.18.577.688.48 3.97-1.32 6.833-5.054 6.833-9.458C22 6.463 17.522 2 12 2Z" fill-rule="evenodd"></path>
             </svg>
             <span>Github</span>
--- client/src/views/LandingIndex.vue
+++ client/src/views/LandingIndex.vue
@@ -7,7 +7,6 @@ import { useNameStore } from "../stores/names.js"
 import BottomBgElement from "../components/Abstract/BottomBgElement.vue"
 import SolidButton from "../components/FormElements/SolidButton.vue"
 
-
 // Names Related
 const nameStore = useNameStore()
 
@@ -77,7 +76,7 @@ const playConfetti = () => {
   <div class="relative px-6 lg:px-8">
     <div class="mx-auto max-w-3xl py-32 sm:py-48 lg:py-56">
       <div class="hidden sm:mb-8 sm:flex sm:justify-center">
-        <div class="relative rounded-full py-1 px-3 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+        <div class="relative rounded-full py-1 px-3 text-sm leading-6 text-gray-600 dark:text-gray-300 ring-1 ring-gray-900/10 dark:ring-gray-500/50 hover:ring-gray-900/20">
           Nepali Baby Name generator from Parent's Name
           <a class="font-semibold text-indigo-600 ml-2" href="#">
             <span aria-hidden="true" class="absolute inset-0"></span>
@@ -87,11 +86,11 @@ const playConfetti = () => {
         </div>
       </div>
       <div class="text-center">
-        <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Random Nepali Names</h1>
+        <h1 class="text-4xl font-bold tracking-tight text-gray-900 dark:text-gray-200 sm:text-6xl">Random Nepali Names</h1>
 
         <div class="mt-8">
-          <div class="w-full max-w-2xl mx-auto border-2 border-dashed rounded-lg h-32 p-8 flex items-center justify-center">
-            <span id="name-placeholder" class="text-5xl">
+          <div class="w-full max-w-2xl mx-auto border-2 border-dashed dark:border-gray-500/50 rounded-lg h-32 p-8 flex items-center justify-center">
+            <span id="name-placeholder" class="text-5xl text-black dark:text-white">
               <span>{{ currentName.name }}</span>
             </span>
           </div>
@@ -106,7 +105,7 @@ const playConfetti = () => {
           <SolidButton
             v-if="previousNames.length > 1 && previousCount !== previousNames.length + 1"
             button-type="transparent"
-            class="md:!w-40 w-full btn"
+            class="md:!w-40 w-full btn dark:bg-black border-transparent dark:border-gray-100/30 dark:text-white"
             @click="getPreviousName"
           >
             <svg class="w-5 h-5" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
@@ -118,7 +117,7 @@ const playConfetti = () => {
           <SolidButton
             v-if="previousCount !== 2"
             button-type="transparent"
-            class="md:!w-40 w-full btn"
+            class="md:!w-40 w-full btn dark:bg-black border-transparent dark:border-gray-100/30 dark:text-white"
             @click="getNextName"
           >
             <span>Next</span>
